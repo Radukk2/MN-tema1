298ed8145693ac0aa6408ac54b9c282efcf44bd6
Radukk2 <115574097+Radukk2@users.noreply.github.com>
2023-04-27 23:09:49 +0300

    Add files via upload

--- decode_path.m
+++ decode_path.m
@@ -9,5 +9,16 @@ function [decoded_path] = decode_path (path, lines, cols)
   % hint: decoded_path does not contain indices for the WIN state
 
   % TODO: decode_path implementation
-
+    lung = length(path);
+    decoded_path = zeros(lung - 1,2);
+    for i=1:lung - 1
+      decoded_path(i,1) = floor(path(i,1) / cols) + 1;
+      if rem(path(i,1), cols) == 0
+        decoded_path(i,1)--;
+      endif
+      decoded_path(i,2) = rem(path(i,1), cols);
+      if rem(path(i,1), cols) == 0
+        decoded_path(i,2) = cols;
+      endif
+    endfor
 endfunction
--- heuristic_greedy.m
+++ heuristic_greedy.m
@@ -8,18 +8,18 @@ function [path] = heuristic_greedy (start_position, probabilities, Adj)
   % TODO: heuristic_greedy implementation
   path = start_position;
   [rows, cols] = size(Adj);
+  probabilities(rows - 1) = 1;
+  probabilities(rows) = 0;
   visited = zeros(rows);
   visited(start_position) = 1;
-  while size(path) != 0
+  while length(path) != 0
     position = path(end);
     if position == rows - 1
-      path;
+      path = transpose(path);
       return;
     endif
     pas = 0;
-    for i=1:rows
-      visited(i)
-      %Adj(position,i)
+    for i=1:rows - 1
       if Adj(position,i) == 1 && visited(i) == 0
         pas++;
         endif
@@ -28,15 +28,15 @@ function [path] = heuristic_greedy (start_position, probabilities, Adj)
        path(end) = [];
      else
        max = -1;
-       for i=1:rows - 2
+       for i=1:rows - 1
          if max < probabilities(i) && Adj(position,i) == 1 && visited(i) == 0
            max = probabilities(i);
          endif
        endfor
-       for i=1:rows - 2
+       for i=1:rows - 1
          if max == probabilities(i) && Adj(position,i) == 1 && visited(i) == 0
            visited(i) = 1;
-           path(end) = i;
+           path(end + 1) = i;
          endif
        endfor
      endif
